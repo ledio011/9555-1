c0457c0909e069e7cc82041115b3f8df1e056f6e
ledio011 <ledionroci857@gmail.com>
2026-09-25 19:08:47 +0200

    Finalize corrected map transition and map_ready sequencing

--- 9555.py
+++ 9555.py
@@ -1955,8 +1955,14 @@ def start_map_transition(conn, picked_char, target_map_id, send_rpc_push, overri
     print("[DEBUG] BEFORE MAP ENTER")
     try:
         ph_p = encode_sproto([(0, 503)])
-        # mapInfoId(0), line_index(1), line_count(2)
-        data = encode_sproto([(0, target_map_id), (1, 0), (2, 1)])
+        # enter_map.response: mapInfoId(0), line_index(1), line_count(2)
+        line_index = max(1, int(picked_char.get('line_index', 1)))
+        line_count = max(line_index, int(picked_char.get('line_count', 3)))
+        data = encode_sproto([
+            (0, target_map_id),
+            (1, line_index),
+            (2, line_count)
+        ])
         pf_p = sproto_pack(ph_p + data)
         conn.sendall(struct.pack(">H", len(pf_p)) + pf_p)
         print(f"[M1003 DEBUG] TX 503 map_id={target_map_id}")
@@ -2297,7 +2303,10 @@ def client_handler(conn, addr):
             elif msg == 100: # map_ready
                 if picked_char:
                     mid = picked_char.get('map_id', '11')
-                    print(f"[MAP READY RECEIVED] map_id={mid}")
+                    picked_char['line_index'] = max(1, int(picked_char.get('line_index', 1)))
+                    picked_char['line_count'] = max(picked_char['line_index'], int(picked_char.get('line_count', 3)))
+                    save_chars(all_accounts_chars)
+                    print(f"[MAP READY RECEIVED] map_id={mid} line={picked_char['line_index']}/{picked_char['line_count']}")
 
                     # 504: main_player_create must be sent after the scene is actually ready.
                     send_rpc_push(504, encode_sproto([
